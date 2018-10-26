99e652fae55ba27e94f65f129e54cd5cc236c9b6
JamesBrown <wck593462925@outlook.com>
2018-10-26 17:12:11 +0800

    GUI Test.

--- interface.py
+++ interface.py
@@ -1,32 +1,60 @@
-import sys
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'interface.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        font = QtGui.QFont()
+        font.setFamily("幼圆")
+        font.setPointSize(10)
+        Form.setFont(font)
+        self.title = QtWidgets.QLabel(Form)
+        self.title.setGeometry(QtCore.QRect(120, 10, 100, 20))
+        font = QtGui.QFont()
+        font.setFamily("幼圆")
+        font.setPointSize(12)
+        self.title.setFont(font)
+        self.title.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+        self.title.setAlignment(QtCore.Qt.AlignCenter)
+        self.title.setObjectName("title")
+        self.original_text = QtWidgets.QTextEdit(Form)
+        self.original_text.setGeometry(QtCore.QRect(20, 80, 120, 80))
+        self.original_text.setObjectName("original_text")
+        self.encrypted_text = QtWidgets.QTextEdit(Form)
+        self.encrypted_text.setGeometry(QtCore.QRect(210, 80, 120, 80))
+        self.encrypted_text.setObjectName("encrypted_text")
+        self.encryption = QtWidgets.QPushButton(Form)
+        self.encryption.setGeometry(QtCore.QRect(40, 160, 80, 24))
+        self.encryption.setObjectName("encryption")
+        self.decryption = QtWidgets.QPushButton(Form)
+        self.decryption.setGeometry(QtCore.QRect(230, 160, 80, 24))
+        self.decryption.setObjectName("decryption")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.title.setText(_translate("Form", "DES加密演示"))
+        self.encryption.setText(_translate("Form", "加密"))
+        self.decryption.setText(_translate("Form", "解密"))
 
 
-class Interface(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(300, 300, 800, 600)
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        quit_button = QPushButton('Quit', self)
-        quit_button.setToolTip('This is a <b>QPushButton</b> widget')
-        quit_button.resize(80, 60)
-        quit_button.move((self.width() - quit_button.width()) / 2, (self.height() - quit_button.height()) / 2)
-        quit_button.clicked.connect(QCoreApplication.instance().quit)
-
-        self.setWindowTitle('DES Encryption Demo')
-        self.setWindowIcon(QIcon('icon.ico'))
-
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Interface()
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
 
